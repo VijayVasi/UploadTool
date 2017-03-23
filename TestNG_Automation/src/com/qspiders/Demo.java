be4347c785e337837c4a57db8f898c2c58e05a2a
Dell <Dell@192.168.2.7>
2017-03-23 09:05:02 +0530

    Uploading Project to Github

---
+++ TestNG_Automation/src/com/qspiders/Demo.java
@@ -0,0 +1,14 @@
+package com.qspiders;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	
+	public void test(){
+		
+		Reporter.log("HI",true);
+	}
+}
---
+++ TestNG_Automation/src/com/qspiders/DemoA.java
@@ -0,0 +1,14 @@
+package com.qspiders;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA {
+	
+	@Test
+	
+	public void testA(){
+		Reporter.log("BYE", true);
+	}
+	
+}
